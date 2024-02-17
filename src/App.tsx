3871f3753759182ce3f817038cf845f94acd3cbe
m.mats <m.mats@andersenlab.com>
2024-02-17 20:06:52 +0200

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { AppRoutes } from "./AppRouter";
